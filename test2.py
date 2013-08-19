1e329d525716f6fbc116c209cd073b0daa5542c4
fedallah <evanjohns@gmail.com>
2013-08-18 20:57:38 -0400

    cookie fix?

--- test2.py
+++ test2.py
@@ -10,13 +10,14 @@ urls = (
 web.config.debug = True
 
 def checkauth():
-	
-app.add_processor(web.loadhook(checkauth))
+	mip = web.cookies(ipaddr=web.ctx.ip)
+	mip.ipaddr
 
 class index:
 	def GET(self):
 		return "hello world"
 
-if __name__ == "__main__":
-	app = web.application(urls, globals())
-	app.run()
+#if __name__ == "__main__":
+app = web.application(urls, globals())
+app.add_processor(web.loadhook(checkauth))
+app.run()
