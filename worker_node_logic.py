50d0621cf95eeeb69c2fed93f741b3a41f378e7b
fedallah <hernia06@gmail.com>
2013-08-27 01:08:00 -0400

    even more work on worker code

--- worker_node_logic.py
+++ worker_node_logic.py
@@ -4,14 +4,22 @@ import web
 import hashlib
 import random
 import time
+import re
 
 rval = 0
 
-try:
-	controldb = web.database(dbn='sqlite', db='/etc/fsart/control')
-except:
-	rval = 1	
+# handle connection to control database.  if it doesn't exist this thing
+# will not start.
+def controlconnect():
+	try:
+		controldb = web.database(dbn='sqlite', db='/fsart/cdb.sqlite3')
+	except:
+		return 1
+	else:
+		return 0
 
+global gmrval = controlconnect()
+		
 urls = (
 	'/', 'status',
 	'/config', 'config',
@@ -27,33 +35,48 @@ web.config.debug = True
 
 app = web.application(urls, globals())
 
+# this problem-checking code will run every time a request is made.
+# is the control db [still] available?  do we have a master configured?
 def hasmaster():
-	if rval = 0:
+	if gmrval == 1:
+		rval = controlconnect()
+	else:	
 		try:
 			masters = controldb.query("SELECT COUNT FROM masters")
-			if masters > 1:
-				rval = 2
+			if masters < 1:
+				rval = 3
 		except:
 			rval = 1
 	if rval > 0:
 		global gmrval = rval
+		# show the error page if there is one
 		raise web.seeother('/err')
 	else:
-		global gmrval = None
-	
-def getstatus():
-	
+		global gmrval = 0
+		
+def ishuman():
+	mq = web.ctx.query
+	if "human" in mq.lower():
+		web.header('Content-type', "text/html; charset=utf-8")
+		
+
+# what is the status of the worker?  is it busy?  what is it doing?
 class status:
+	mq = web.ctx.query
+	if "human" in mq.lower():
 
 class error:
-	if web.ctx.query = human:
+	def GET(self):
+		mq = web.ctx.query
+		if "human" in mq.lower():
 	
 	else:
 		return gmrval
 	
 class job_cancel:
 	
-app.add_processor(web.loadhook(getstatus))
+app.add_processor(web.loadhook(hasmaster))
+app.add_processor(ishuman)
 	
 if __name__ == "__main__":
 	app.run()
